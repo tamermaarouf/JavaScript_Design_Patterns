871742bc44e41c10f5dad5658a5f4cf49d511766
tamer_maarouf <tamermaarouf@gmail.com>
2022-05-26 21:28:38 +0200

    add more cat to ko.observableArray knockoutJS

--- cat-clicker-Knockoutjs/js/app.js
+++ cat-clicker-Knockoutjs/js/app.js
@@ -1,32 +1,81 @@
-let cats = function(){
-    this.name = ko.observable('Tabby');
-    this.clickCount = ko.observable(0);
+const initialCats = [
+  {
+    name: "Cornish Rex",
+    clickCount: 0,
+    imgSrc: "https://live.staticflickr.com/5227/5616777560_d745f938a9_z.jpg",
+    nicknames: ["Cornish Rex", "Ragdoll", "Toyger", "Cat four", "Cat five"]
+  },
+  {
+    name: "Ragdoll",
+    clickCount: 0,
+    imgSrc: "https://live.staticflickr.com/65535/51649693392_468c8cf0a1_z.jpg",
+    nicknames: ["Cornish Rex", "Ragdoll", "Toyger", "Cat four", "Cat five"]
+  },
+  {
+    name: "Toyger",
+    clickCount: 0,
+    imgSrc: "https://live.staticflickr.com/2624/3892070704_b101db0c7d_z.jpg",
+    nicknames: ["Cornish Rex", "Ragdoll", "Toyger", "Cat four", "Cat five"]
+  },
+  {
+    name: "Cat four",
+    clickCount: 0,
+    imgSrc: "https://live.staticflickr.com/5227/5616777560_d745f938a9_z.jpg",
+    nicknames: ["Cornish Rex", "Ragdoll", "Toyger", "Cat four", "Cat five"]
+  },
+  {
+    name: "Cat five",
+    clickCount: 0,
+    imgSrc: "https://live.staticflickr.com/5227/5616777560_d745f938a9_z.jpg",
+    nicknames: ["Cornish Rex", "Ragdoll", "Toyger", "Cat four", "Cat five"]
+  }
+];
 
+let Cat = function (data) {
+  this.name = ko.observable(data.name);
+  this.clickCount = ko.observable(data.clickCount);
+  this.imgSrc = ko.observable(data.imgSrc);
 
-    this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
+  this.nicknames = ko.observableArray(data.nicknames);
 
-    this.cats = ko.observableArray(['Cornish Rex', 'Ragdoll', 'Toyger', 'Cat four', 'Cat five']);
-
-    this.title = ko.computed(function () {
-        let title;
-        let clicks = this.clickCount();
-        if (clicks <= 10) {
-            title = 'Newborn';
-        } else if (clicks < 50) {
-            title = 'infant';
-        } else if (clicks < 100) {
-            title = 'Child';
-        }
-        return title;
-    }, this);
-
-}
+  this.title = ko.computed(function () {
+    let title;
+    let clicks = this.clickCount();
+    if (clicks <= 10) {
+      title = "Newborn";
+    } else if (clicks < 50) {
+      title = "infant";
+    } else if (clicks < 100) {
+      title = "Child";
+    }
+    return title;
+  }, this);
+};
 
 let viewModel = function () {
-    this.currentCat = ko.observable( new cats() );
 
-    this.incrementCounter = function () {
-        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
-    };
+  let self = this;
+
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach((catItem) => {
+    this.catList.push(new Cat(catItem));
+
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
+
+  this.incrementCounter = function () {
+    this.clickCount(this.clickCount() + 1);
+  };
+
+  this.catListItem = function (data) {
+    const index = self.currentCat().nicknames().findIndex(name => name === data);
+
+    self.currentCat(self.catList()[index]);
+  };
+
+
 };
+
 ko.applyBindings(new viewModel());
